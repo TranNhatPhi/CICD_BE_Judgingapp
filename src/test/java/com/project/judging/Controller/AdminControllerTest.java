74b1e6898d64622307461655502cb76eb9f78a24
TranNhatPhi <phivt1234@gmail.com>
2025-03-07 22:03:33 +0700

    feat : AdminControllerTest

---
+++ src/test/java/com/project/judging/Controller/AdminControllerTest.java
@@ -0,0 +1,10 @@
+package com.project.judging.Controller;
+
+
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+@WebMvcTest(controllers = AdminController.class)
+public class AdminControllerTest {
+
+
+}
