8351d3df3e9439eae7501e7fa5083966115c227e
TranNhatPhi <phivt1234@gmail.com>
2025-03-19 19:49:48 +0700

    modify

--- src/main/java/com/project/judging/Config/Security/SecurityConfig.java
+++ src/main/java/com/project/judging/Config/Security/SecurityConfig.java
@@ -232,8 +232,8 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("*"));
-        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
+        configuration.setAllowedOriginPatterns(List.of("*"));
+        configuration.setAllowedMethods(List.of("*"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
